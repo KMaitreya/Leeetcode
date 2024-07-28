9ecc47584b65717cfdc47e6226b55b862bbc56e0
Maitreya Kanitkar <maitreya.kanitkar@gmail.com>
2024-07-28 02:48:04 +0000

    Code challenge submission for question: 3232 Find if Digit Game Can Be Won

---
+++ 3232 Find if Digit Game Can Be Won/3232find-if-digit-game-can-be-won.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canAliceWin(self, nums: List[int]) -> bool:
+        
+        singleSum=0
+        doubleSum=0
+
+        for num in nums:
+            if num<10:
+                singleSum+=num
+            else:
+                doubleSum+=num
+
+        if singleSum==doubleSum:
+            return False
+        return True
