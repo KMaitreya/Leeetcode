e12524a506563833809d056cc74245110ef7e9bf
Maitreya Kanitkar <maitreya.kanitkar@gmail.com>
2024-10-11 22:51:32 +0000

    Code challenge submission for question: 100 Same Tree

---
+++ 100 Same Tree/100same-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        
+        # def traverse(p, q):
+        #     if p is None and q is None:
+        #         return True
+        #     if p is None or q is None:
+        #         return False
+        #     if p and q:
+        #         print(p.val, q.val)
+        #         if p.val!=q.val:
+        #             return False
+        #         return traverse(p.left, q.left) and traverse(p.right, q.right)
+
+        # return traverse(p, q)
+
+        if p is None and q is None:
+            return True
+        if p is None or q is None or p.val!=q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
