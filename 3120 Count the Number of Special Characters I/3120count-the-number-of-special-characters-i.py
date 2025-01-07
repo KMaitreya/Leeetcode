c6fbeeea4190081609ffb823fdc226cb4d6fcd0b
Maitreya Kanitkar <maitreya.kanitkar@gmail.com>
2025-01-07 06:02:29 +0000

    Code challenge submission for question: 3120 Count the Number of Special Characters I

---
+++ 3120 Count the Number of Special Characters I/3120count-the-number-of-special-characters-i.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+
+        word=set(word)
+        cnt=0
+        
+        hmap = { 'a': 'A', 'b': 'B', 'c': 'C', 'd': 'D', 'e': 'E', 'f': 'F', 'g': 'G', 'h': 'H', 'i': 'I', 'j': 'J', 'k': 'K', 'l': 'L', 'm': 'M', 'n': 'N', 'o': 'O', 'p': 'P', 'q': 'Q', 'r': 'R', 's': 'S', 't': 'T', 'u': 'U', 'v': 'V', 'w': 'W', 'x': 'X', 'y': 'Y', 'z': 'Z'}
+        for key, value in hmap.items():
+            if key in word and value in word:
+                cnt+=1
+
+        return cnt
