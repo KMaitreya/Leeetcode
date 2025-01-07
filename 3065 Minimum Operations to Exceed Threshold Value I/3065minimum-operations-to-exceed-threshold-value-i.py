950298c0f6645c6a960ed2e81ff8f88df8bed4bc
Maitreya Kanitkar <maitreya.kanitkar@gmail.com>
2025-01-07 22:11:14 +0000

    Code challenge submission for question: 3065 Minimum Operations to Exceed Threshold Value I

---
+++ 3065 Minimum Operations to Exceed Threshold Value I/3065minimum-operations-to-exceed-threshold-value-i.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        
+        cnt=0
+        
+        for num in nums:
+            if num<k:
+                cnt+=1
+
+        return cnt
